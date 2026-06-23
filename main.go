f4ea3778e5fa7ecd7fbb94114f154df5c1789661
kamalkn24 <kamalkn@jfrog.com>
2026-06-24 04:28:56 +0530

    Create main.go

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    _ "github.com/dgrijalva/jwt-go"
+    _ "github.com/gin-gonic/gin"
+    _ "github.com/gogo/protobuf/proto"
+    _ "gopkg.in/yaml.v2"
+    _ "github.com/tidwall/gjson"
+)
+
+func main() {}
